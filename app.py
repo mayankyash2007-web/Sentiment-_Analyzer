3fe59794ea67be34422c85e49c0d1440689ed4eb
Mayank Shrivastava <mayankyash2007@gmail.com>
2026-03-29 21:56:03 +0530

    Implement sentiment prediction using a trained model
    Load model and vectorizer using pickle and add sentiment prediction functionality.

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
+import pickle
 
+model = pickle.load(open("model/model.pkl", "rb"))
+vectorizer = pickle.load(open("model/vectorizer.pkl", "rb"))
+
+def predict_sentiment(text):
+    text_vector = vectorizer.transform([text])
+    prediction = model.predict(text_vector)
+    return prediction[0]
+
+# Test
+while True:
+    text = input("Enter text: ")
+    print("Sentiment:", predict_sentiment(text))
