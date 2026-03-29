49535d5d11a267951ef24f70513404ad7b42ed0a
Mayank Shrivastava <mayankyash2007@gmail.com>
2026-03-29 22:01:28 +0530

    Integrate text preprocessing in sentiment prediction
    Added text preprocessing step before sentiment prediction.

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import pickle
+from utils import preprocess_text
 
 model = pickle.load(open("model/model.pkl", "rb"))
 vectorizer = pickle.load(open("model/vectorizer.pkl", "rb"))
 
 def predict_sentiment(text):
+    text = preprocess_text(text)
+    
     text_vector = vectorizer.transform([text])
     prediction = model.predict(text_vector)
     return prediction[0]
